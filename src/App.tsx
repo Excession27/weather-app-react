9917dc9d717437a681b2166149ae7bc9f653b3ec
Excession27 <feboobef27@gmail.com>
2022-06-23 10:53:45 +0200

    Husky+lint-staged test#2

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import Home from "container/Home";
 import SearchContextProvider from "hoc/SearchContextProvider";
 
-const unusedVar = "";
-const anotherUnused = "";
-
+const a = 1;
+const b = 2;
+const unused = a == b;
+function A() {
+  if (unused) {
+    console.log("it's true");
+  }
+}
+A();
 function App() {
   return (
     <SearchContextProvider>
