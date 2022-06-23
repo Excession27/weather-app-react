9454c5cc8cd7338b8899f5e48bddfcb59dfd6ac3
Excession27 <feboobef27@gmail.com>
2022-06-23 10:10:19 +0200

    Husky+lint-staged test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "container/Home";
 import SearchContextProvider from "hoc/SearchContextProvider";
-
+const unusedVar = "";
 function App() {
   return (
     <SearchContextProvider>
